6f40669ccbc0a4ab9142fa6d61f8faca49248084
AJEasy <aje600@gmail.com>
2017-03-28 14:36:47 -0400

    Create about.php

---
+++ includes/content/about.php
@@ -0,0 +1,15 @@
+<div class="about">
+	<div class="container standardsp ">
+		<div class="row topspace bottomspace">
+			<div class="container standardsp-mar-sm homepage">
+			<div class="col-sm-10 col-sm-offset-1 standardsp-mar-sm">
+				<h1>About Flywing Designs...</h1>
+				<div class="standardsp-mar-sm jus-text">
+				<p class="text18">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis ornare nisi. Cras ultrices metus enim, eu semper mi iaculis volutpat. Donec velit erat, dapibus ac fringilla sit amet, venenatis vehicula nibh. Integer sit amet elementum tellus. Curabitur vel magna suscipit, lacinia eros quis, maximus metus. Donec a purus porttitor, consectetur purus sit amet, varius purus. Vivamus vel ornare eros. Cras facilisis volutpat ultrices. Praesent imperdiet quis enim quis ultricies. Integer nunc orci, tempor nec ullamcorper sed, fringilla et eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis ornare nisi. Cras ultrices metus enim, eu semper mi iaculis volutpat. Donec velit erat, dapibus ac fringilla sit amet, venenatis vehicula nibh. Integer sit amet elementum tellus. Curabitur vel magna suscipit, lacinia eros quis, maximus metus. Donec a purus porttitor, consectetur purus sit amet, varius purus. Vivamus vel ornare eros. Cras facilisis volutpat ultrices. Praesent imperdiet quis enim quis ultricies. Integer nunc orci, tempor nec ullamcorper sed, fringilla et eros.</p>
+				<p class="text18">In maximus urna tortor, non cursus libero rutrum euismod. Fusce dictum sodales tortor ut suscipit. Duis iaculis mauris eu nulla placerat, sit amet molestie arcu efficitur. Ut vel quam lobortis, semper tortor in, lacinia felis. Phasellus neque lacus, semper vel pretium vitae, rutrum eu dui. In arcu quam, bibendum quis dolor vel, consequat egestas justo. Donec vel diam non ante aliquet efficitur. Proin luctus porta nibh et sollicitudin. In maximus urna tortor, non cursus libero rutrum euismod. Fusce dictum sodales tortor ut suscipit. Duis iaculis mauris eu nulla placerat, sit amet molestie arcu efficitur. Ut vel quam lobortis, semper tortor in, lacinia felis. Phasellus neque lacus, semper vel pretium vitae, rutrum eu dui. In arcu quam, bibendum quis dolor vel, consequat egestas justo. Donec vel diam non ante aliquet efficitur. Proin luctus porta nibh et sollicitudin.</p>
+			</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
